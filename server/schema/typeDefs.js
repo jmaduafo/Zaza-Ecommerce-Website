6e30b5a456b64f41f5b9ff934deb43111bdebdd7
ctalv <122413805+ctalv@users.noreply.github.com>
2023-07-31 13:23:09 -0500

    added base typeDefs

--- server/schema/typeDefs.js
+++ server/schema/typeDefs.js
@@ -5,8 +5,35 @@ const typeDefs = gql`
     _id: ID
     username: String
     email: String
-    # There is now a field to store the user's password
     password: String
+    order: [Product]
+  }
+
+  type Category {
+    _id: ID
+    name: String
+    subcategories: [SubCategory]
+  }
+
+  type SubCategory {
+    _id: ID
+    name: String
+    products: [Product]
+  }
+
+  type Product {
+    _id: ID
+    name: String!
+    category: Category
+    subcategory: SubCategory
+    stock: Int
+    price: Int
+  }
+
+  type Order {
+    _id: ID
+    user: User!
+    items: [Products]
   }
 
   # Set up an Auth type to handle returning data from a profile creating or user login

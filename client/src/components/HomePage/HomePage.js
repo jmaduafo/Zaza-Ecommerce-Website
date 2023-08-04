0873d271518afd2c423b4af7dfd933d1cc6cbeff
Edith <rissafonseca@yahoo.com>
2023-08-03 21:27:44 -0500

    fixed p tags issue

--- client/src/components/HomePage/HomePage.js
+++ client/src/components/HomePage/HomePage.js
@@ -6,22 +6,22 @@ import lingerieSet from '../../assets/images/lingerieset.jpeg';
 import pajamaSet from '../../assets/images/pajamassetjpg.jpg';
 import { Icon } from '@iconify/react';
 
-export default function HomePage() {
+function HomePage() {
     return (
         <div className="homepage-wrapper">
             <div className="border-with-icons">
             <div className="icon-container">
             <div className="icon-item">
             <Icon icon="mdi:truck-shipping" width="44" />
-            <p>Online shipping and discount prices on</p>
+            <p className='icon-p'>Online shipping and discount prices on</p>
             </div>
             <div className="icon-item">
             <Icon icon="majesticons:creditcard-line" width="44"/>
-            <p>We ensure secure payments at checkout</p>
+            <p className='icon-p'>We ensure secure payments at checkout</p>
             </div>
             <div className="icon-item">
             <Icon icon="mdi:user" width="44"/>
-            <p>Private information of our users is always protected</p>
+            <p className='icon-p'>Private information of our users is always protected</p>
             </div>
             </div>
             </div>
@@ -57,3 +57,5 @@ export default function HomePage() {
 
     );
 }
+
+export default HomePage

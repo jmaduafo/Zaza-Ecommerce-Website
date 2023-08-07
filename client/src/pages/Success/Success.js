6cc8b2fa089bc57dac2813fe355413a7bcc70e93
Jay Madu <jmaduafokwa@hotmail.com>
2023-08-07 02:51:52 +0100

    added reducers and idbpromises to cart summary, success, and navbar, got success page to redirect after items are saved, and added some reducers to cart.js

--- client/src/pages/Success/Success.js
+++ client/src/pages/Success/Success.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import './success.css'
+import { useMutation } from '@apollo/client';
+import { ADD_ORDER } from '../../utils/mutations';
+import { idbPromise } from '../../utils/helpers';
 
 import { Link } from 'react-router-dom'
 
 const Success = () => {
+  const [addOrder] = useMutation(ADD_ORDER);
+
+  useEffect(() => {
+    async function saveOrder() {
+      const cart = await idbPromise('cart', 'get');
+      const products = cart.map((item) => item._id);
+
+      if (products.length) {
+        const { data } = await addOrder({ variables: { products } });
+        const productData = data.addOrder.products;
+
+        productData.forEach((item) => {
+          idbPromise('cart', 'delete', item);
+        });
+      }
+
+      setTimeout(() => {
+        window.location.assign('/');
+      }, 3000);
+    }
+
+    saveOrder();
+  }, [addOrder]);
+
   return (
     <div className='success-page'>
       <div className='success-card'>
@@ -13,7 +40,7 @@ const Success = () => {
           </div>
         </div>
         <div className='bottom-portion'>
-          <p>Payment was successful!</p>
+          <p>Payment was successful! If this<br/> page doesn't redirect, please click the button below.</p>
           <Link to='/'><button>RETURN HOME</button></Link>
         </div>
       </div>

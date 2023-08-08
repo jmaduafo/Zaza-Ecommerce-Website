26b1dd34fd6d965d0d54e7dad3bcf2f64473326f
ctalv <122413805+ctalv@users.noreply.github.com>
2023-08-08 17:33:53 -0500

    deployment changes

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -117,6 +117,8 @@ const Navbar = ({ setNavClick, navClick }) => {
   if (lodingCategories || loadingSubcategories) {
     return <Loader/>;
   }
+
+  console.log(categoryData)
   
   function calculateTotalItems() {
     let sum = 0;
